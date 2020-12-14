fcbbf48a540ee4f2bf3cde4a337f98a765f60717
DEV <dev@info.local>
2020-12-14 15:30:17 -0300

    Exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Digite seu nome: ");
+           string a = Console.ReadLine();
+
+           string resultado = "\r\nBem vindo, " + a;
+
+           Console.WriteLine(resultado);
+        }
+    }
+}
